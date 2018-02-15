d33e30d2e3420717e48078c5e9b0cb8e17b67d41
lilithakobyan <lilithakobyan1994@mail.ru>
2018-02-15 15:32:22 +0400

    Map and MapWhen

--- FirstWebApplication/FirstWebApplication/Startup.cs
+++ FirstWebApplication/FirstWebApplication/Startup.cs
@@ -25,10 +25,40 @@ namespace FirstWebApplication
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Map("/home", home =>
+            {
+                
+                home.Map("/index", Index);
+                home.Map("/about", About);
+            });
+           
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Page Not Found");
             });
+        }
+        private void Index(IApplicationBuilder app)
+        {
+            app.MapWhen(context => {
+
+                return context.Request.Query.ContainsKey("id") &&
+                       context.Request.Query["id"] == "5";
+            }, HandleId);
+            app.Run(async context=> await  context.Response.WriteAsync("<h1>Index</h1>")
+            );
+        }
+        //http://localhost:49246/home/index/product?id=5&name=phone
+        private static void HandleId(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("id is equal to 5");
+            });
+        }
+        private void About(IApplicationBuilder app)
+        {
+            app.Run(async context=> await  context.Response.WriteAsync("<h1>About</h1>")
+            );
         }
     }
 }
